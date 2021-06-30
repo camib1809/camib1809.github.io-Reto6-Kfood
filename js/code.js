661013c56e37eea9e0b61b25dc35a6fc44a38f29
camib1809 <you@example.com>
2021-06-29 20:31:28 -0500

    terminado

--- js/code.js
+++ js/code.js
@@ -12,3 +12,129 @@ let x = 0;
             x = 0;
         }
 });
+
+//Variables 
+
+const addToShoppingCartButtons = document.querySelectorAll('.Agregar');
+const shoppingCartItemsContainer = document.querySelector('#shoppingContainer');
+const removeAllItems = document.querySelector('#clearAll');
+const msmCar = document.getElementById('textcar');
+const VerMas = document.querySelector('.ver')
+const textMas = document.querySelector('#mas')
+const productos = document.querySelectorAll('.productos');
+
+// Actualiza el número total de productos en carrito
+
+const totalCarrito = document.querySelector("#items");
+totalCarrito.textContent = totalProductos();
+
+// Eventos
+
+addToShoppingCartButtons.forEach(addToCartButton => {
+    addToCartButton.addEventListener('click', addToCartClicked);
+});
+shoppingCartItemsContainer.addEventListener('click', removeShoppingCartItem);
+removeAllItems.addEventListener("click", clearItems);
+VerMas.addEventListener("click", VerTodo);
+
+function addToCartClicked(event){
+    const button = event.target;
+    const card = button.closest('.card');
+    
+    const cardTitle = card.querySelector('#title').textContent;
+    const cardPrice = card.querySelector('#price').textContent;
+    const cardImg = card.querySelector('.prod-img').src;
+    // console.log('addToCartClicked -> cardPrice', cardTitle, cardPrice, cardImg);
+
+    addItemToShoppingCart(cardTitle, cardPrice, cardImg);
+}
+
+function addItemToShoppingCart(cardTitle, cardPrice, cardImg){
+    const shoppingCartRow = document.createElement('div');
+    totalCarrito.textContent = totalProductos();
+    const shoppingCartContent = `
+    <div class="row d-flex flex-row px-5 py-3 border-bottom">
+    <div class="col-11 col-sm-11 d-flex flex-column flex-sm-row justify-content-center align-items-center">
+        <img src=${cardImg} class="col-11 col-sm-2 mr-0 mr-sm-3">
+        <div class="col-12 col-sm-5 text-center d-flex flex-column justify-content-center ml-0 ml-sm-5 mt-2 mt-sm-0">
+          <h4>${cardTitle}</h4>
+        </div>
+        <div class="col-12 col-sm-3 text-center ml-0 ml-sm-5 d-flex align-items-center justify-content-center">
+          <h4>${cardPrice}</h4>
+        </div>
+    </div>
+    <div class="d-flex align-items-center cursor-pointer">
+        <img src="Images/icon-cross.svg" class="cross">
+    </div>
+</div>`;
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+
+    shoppingCartRow.querySelector('.cross').addEventListener('click', removeShoppingCartItem);
+
+    totalCarrito.textContent = totalProductos();
+
+    // Condicional para eliminar 
+        msmCar.classList.add("d-none");
+        msmCar.classList.remove("d-flex");
+    
+}
+
+function removeShoppingCartItem(event){
+    if(event.target.classList.contains("cross")){    
+        event.target.parentElement.parentElement.parentElement.remove();
+        totalCarrito.textContent = totalProductos();
+        console.log();
+
+        
+    }
+    // Condicional para eliminar 
+    if(totalProductos() == 0){
+        msmCar.classList.add("d-flex");
+        msmCar.classList.remove("d-none");
+    }
+}
+
+function clearItems(event){
+    const productosCarrito = Array.from(shoppingCartItemsContainer.children);
+    console.log(productosCarrito);
+
+    let arrayCompleted = productosCarrito;
+
+    arrayCompleted.forEach(function(element, i){
+        arrayCompleted[i].remove();
+    });
+
+    totalCarrito.textContent = totalProductos();
+
+    // Condicional para eliminar 
+    msmCar.classList.add("d-flex");
+    msmCar.classList.remove("d-none");
+}
+
+function totalProductos(){
+
+    const productosCarrito = Array.from(shoppingCartItemsContainer.children);
+    console.log(productosCarrito);
+
+    let totalProductos = productosCarrito;
+
+    return totalProductos.length;
+
+}
+
+// boton de ver más
+
+function myFunction() {
+    var x = document.getElementById("productos");
+    if (x.classList == "d-none") {
+        x.classList.add("d-flex");
+        x.classList.remove("d-none");
+    } else {
+        x.classList.add("d-flex");
+    }
+}
+
+
+
+    
